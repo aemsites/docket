979a1829f2560f4dc7fb642dcb50d34ec295592e
Chris Millar <cmillar@adobe.com>
2025-04-22 16:35:49 -0600

    Switch to prism

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -1,20 +1,36 @@
+import { loadStyle } from '../../scripts/nx.js';
 import observe from '../../scripts/utils/intOb.js';
 
-async function decorate(el) {
-  const { codeToHtml } = await import('https://esm.sh/shiki@3.2.2');
+let prism;
+const languages = {
+  javascript: { name: 'javascript' },
+  curl: { name: 'bash' },
+};
 
-  const titleRows = el.querySelectorAll(':scope > div:nth-child(odd)');
-  titleRows.forEach(async (row) => {
+async function decorate(el) {
+  if (!prism) prism = import('prismjs');
+  await prism;
+
+  const titleRows = [...el.querySelectorAll(':scope > div:nth-child(odd)')];
+  const highlights = titleRows.map(async (row) => {
     const type = row.textContent.toLowerCase().trim();
-    const lang = type === 'javascript' ? 'js' : 'bash';
+    const lang = languages[type];
+    if (!lang) throw Error('Language not supported');
+
+    if (!lang.module) lang.module = import(`/deps/prismjs/components/prism-${lang.name}.js`);
+    await lang.module;
 
     const pre = row.nextElementSibling.querySelector('pre');
+    const code = pre.querySelector('code');
+    code.classList.add(`language-${lang.name}`);
 
-    pre.parentElement.innerHTML = await codeToHtml(pre.innerText, {
-      lang,
-      theme: 'slack-dark',
+    return new Promise((resolve) => {
+      window.Prism.highlightElement(code, false, () => { resolve(); });
     });
   });
+
+  await Promise.all(highlights);
+  el.classList.add('is-highlighted');
 }
 
 export default async function init(el) {
