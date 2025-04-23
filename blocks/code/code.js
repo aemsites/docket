9c660319b1b8c464dfd64a354f58412ced7fd13d
Chris Millar <cmillar@adobe.com>
2025-04-22 21:05:45 -0600

    WIP

--- blocks/code/code.js
+++ blocks/code/code.js
@@ -7,14 +7,26 @@ const languages = {
   css: { name: 'css' },
 };
 
-function decorateTabs(tabsRow) {
+function decorateTabs(tabsRow, panels) {
   tabsRow.classList.add('code-tabs-container');
 
   const tabsList = tabsRow.querySelector('ul');
   tabsList.classList.add('code-tabs-list');
 
   const tabs = [...tabsList.querySelectorAll('li')];
-  return tabs.map((tab) => tab.textContent.toLowerCase().trim());
+  return tabs.map((tab, index) => {
+    if (index === 0) tab.classList.add('is-active');
+    tab.addEventListener('click', () => {
+      tabs.forEach((offTab, idx) => {
+        offTab.classList.remove('is-active');
+        panels[idx].classList.remove('is-active');
+      });
+      tab.classList.add('is-active');
+      panels[index].classList.add('is-active');
+    });
+
+    return tab.textContent.toLowerCase().trim();
+  });
 }
 
 async function decorate(el) {
@@ -23,9 +35,17 @@ async function decorate(el) {
   const rows = [...el.querySelectorAll(':scope > div')];
 
   const tabsRow = rows.shift();
-  const tabsArr = decorateTabs(tabsRow);
+  const tabsArr = decorateTabs(tabsRow, rows);
+
+  const tabPanel = document.createElement('div');
+  tabPanel.classList.add('code-tabs-panel-container');
+  el.append(tabPanel);
 
   const highlights = rows.map(async (row, idx) => {
+    if (idx === 0) row.classList.add('is-active');
+
+    tabPanel.append(row);
+    row.classList.add('code-tabs-panel');
     const type = tabsArr[idx];
     const lang = languages[type];
     if (!lang) throw Error('Language not supported');
