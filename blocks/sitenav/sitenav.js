49764f98351df734e46be0030f8c32652aacfd48
Chris Millar <cmillar@adobe.com>
2025-04-27 15:46:10 -0600

    WIP

--- blocks/sitenav/sitenav.js
+++ blocks/sitenav/sitenav.js
@@ -1,10 +1,15 @@
-export default function init(el) {
+import { getConfig } from '../../scripts/nx.js';
+import getSvg from '../../scripts/utils/svg.js';
+
+export default async function init(el) {
+  const { codeBase } = getConfig();
   const link = document.createElement('a');
   link.href = '/';
   link.className = 'docket-brand-logo';
-  const img = document.createElement('img');
-  img.src = '/img/main-logo-white.svg';
-  link.append(img);
+  link.setAttribute('aria-label', 'Home');
+
+  const svg = await getSvg({ paths: [`${codeBase}/img/logos/site.svg`] });
+  link.append(svg[0]);
 
   el.append(link);
 }
