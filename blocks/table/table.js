16753d032ce80724a67c119908325dce2cd9f838
Chris Millar <cmillar@adobe.com>
2025-06-03 22:23:13 -0600

    Minor styling fixes

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -2,16 +2,7 @@ export default function init(el) {
   const rows = [...el.querySelectorAll('tr')];
   const headingRow = rows.shift();
   headingRow.classList.add('table-heading-row');
-  rows.forEach((row) => {
+  for (const row of rows) {
     row.classList.add('table-content-row');
-    const cells = row.querySelectorAll('td');
-    cells.forEach((cell) => {
-      let code = cell.querySelector('code');
-      if (code) return;
-      code = document.createElement('code');
-      code.textContent = cell.textContent;
-      cell.innerHTML = '';
-      cell.append(code);
-    });
-  });
+  }
 }
