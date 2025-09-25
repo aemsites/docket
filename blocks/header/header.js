509c6623a523178b96b17753759392a40d90208d
Markus Haack <mhaack@adobe.com>
2025-09-25 12:17:18 +0200

    fix: move search setup to dynamic config

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,8 +1,6 @@
-import { getConfig, getMetadata } from '../../scripts/nx.js';
+import { getConfig, getMetadata, loadStyle } from '../../scripts/nx.js';
 import getSvg from '../../scripts/utils/svg.js';
 import { loadFragment } from '../fragment/fragment.js';
-import decorateSearchPanel from './search.js';
-import loadScript from '../../scripts/utils/script.js';
 
 const { locale, codeBase } = getConfig();
 
@@ -57,6 +55,39 @@ async function decorateActions(section) {
   await Promise.all([color, discord, github]);
 }
 
+async function decorateSearch(actions) {
+  try {
+    // fetch site config
+    const configReq = await fetch('/config.json');
+    if (!configReq.ok) {
+      return;
+    }
+    const configData = await configReq.json();
+    if (!configData.public || !configData.public.search) {
+      return;
+    }
+
+    const searchConfig = { ...configData.public.search };
+
+    // init search
+    const search = document.createElement('div');
+    search.id = 'search';
+    actions.before(search);
+
+    loadStyle('https://cdn.jsdelivr.net/npm/@docsearch/css@4.0.1');
+    const docsearch = await import('https://cdn.jsdelivr.net/npm/@docsearch/js@4.0.1/+esm');
+    docsearch.default({
+      container: search,
+      appId: searchConfig.agolia_appId,
+      apiKey: searchConfig.agolia_appKey,
+      indexName: searchConfig.agolia_indices[0],
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
+
 async function decorateHeader(fragment) {
   const img = fragment.querySelector('.section:first-child img');
   if (img) {
@@ -73,11 +104,7 @@ async function decorateHeader(fragment) {
   // Only decorate the action area if it has not been decorated
   if (actions?.classList.length < 2) await decorateActions(actions);
 
-  // Decorate the search panel
-  const search = document.createElement('div');
-  search.id = 'docsearch';
-  actions.before(search);
-  decorateSearchPanel(search);
+  decorateSearch(actions);
 }
 
 /**
