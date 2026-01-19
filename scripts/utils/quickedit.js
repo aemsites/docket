35d77c632f1bc06e08311be3922b6c617e3c2653
Markus Haack <mhaack@adobe.com>
2026-01-19 16:10:50 +0100

    fix: update import map

--- scripts/utils/quickedit.js
+++ scripts/utils/quickedit.js
@@ -1,20 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { loadPage } from '../scripts.js';
 
-const importMap = {
-  imports: {
-    'da-lit': 'https://da.live/deps/lit/dist/index.js',
-    'da-y-wrapper': 'https://da.live/deps/da-y-wrapper/dist/index.js',
-  },
-};
-
-function addImportmap() {
-  const importmapEl = document.createElement('script');
-  importmapEl.type = 'importmap';
-  importmapEl.textContent = JSON.stringify(importMap);
-  document.head.appendChild(importmapEl);
-}
-
 async function loadModule(origin, payload) {
   document.body.classList.add('quick-edit');
   const { default: loadQuickEdit } = await import(`${origin}/nx/public/plugins/quick-edit/quick-edit.js`);
@@ -22,6 +8,5 @@ async function loadModule(origin, payload) {
 }
 
 export default function init(payload) {
-  addImportmap();
   loadModule('https://da.live', payload);
 }
